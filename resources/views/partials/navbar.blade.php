27a1fa6a6b0ed9c49ef67ce5c29121378297efeb
Mahomedalid Pacheco <mictlan@gmail.com>
2017-01-30 05:53:01 +0000

    Adding navbar i18n

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -12,7 +12,7 @@
 
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}">
-                Lara POS
+                {{ trans('app.name') }}
             </a>
         </div>
 
